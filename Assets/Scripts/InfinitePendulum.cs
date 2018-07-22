b26e08289b9b9ab32492e7dea6c4c7a294a5ca95
Alejosaur <asantamariap@unal.edu.co>
2018-07-22 11:08:13 -0500

    pendulo

---
+++ Assets/Scripts/InfinitePendulum.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfinitePendulum : MonoBehaviour {
+
+	private bool bajando;
+    private Rigidbody rb;
+	public float trust;
+	private float y;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		bajando = true;
+		y = transform.position.y;
+		Debug.Log(y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bajando && transform.position.y > y)
+        {
+            bajando = false;
+            rb.AddForce(rb.velocity * trust);
+        }
+        if (!bajando && transform.position.y <y)
+        {
+			Debug.Log(transform.rotation);
+            bajando = true;
+        }
+        y = transform.position.y;
+	}
+}
